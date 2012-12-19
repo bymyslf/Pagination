4a46e82813ef9c8f9a3751d3e67af3b8852b51b9
Luís Barbosa <luisbarbosa@outlook.com>
2012-12-19 08:59:18 -0800

    MySqlPagination

---
+++ MySqlPagination.php
@@ -0,0 +1,48 @@
+<?php
+  class MySqlPagination extends Pagination 
+    {
+        public function __construct(array $options) {
+  		$this = array_merge($this, $options);
+			
+			if (empty($this['baseLink'])) {
+				$this['baseLink'] = basename(htmlspecialchars($_SERVER['PHP_SELF']));
+			}
+		}
+		
+		public static function factory(array $options) {
+			return new MySqlPagination($options);
+		}
+        
+        public function rows() {
+			if (!is_null($this['connection']) && !is_resource($this['connection'])) {
+				if ($this['debug']) {
+                    throw new RuntimeException('Check if the provided sql connection is a valid resource!');
+				}	
+				return false;
+			}
+            
+            $resultTotal = mysql_query($this['sqlStatement'], $this['connection']);
+            $this->rowCount = mysql_num_rows($resultTotal);
+			if ($this->rowCount == 0) {
+				if ($this['debug']) {
+                    throw new RuntimeException('Query returned zero rows.');
+				}
+				return false;
+			}
+            
+            if ($this['page'] === 'all') {
+				$paginationQuery = sprintf('%s %s', $this['sqlStatement'], $this['orderBy']);
+			} else {
+                $page = (int)$this['page'];
+                $itemsPerPage = (int)$this['itemsPerPage'];
+                
+				$this->totalPages = ceil($this->rowCount / $itemsPerPage);
+				$limitBegin = (($page - 1) * $itemsPerPage) + 1;
+				
+                $paginationQuery = sprintf('%s LIMIT %u, %u', $this['sqlStatement'], $limitBegin, $itemsPerPage);
+			}
+            
+			return mysql_query($paginationQuery, $this['connection']);
+		}
+    }
+?>
